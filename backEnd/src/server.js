87162e8c16153945e9de71f80c19d04505379122
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2025-06-24 23:31:05 -0700

    Add player routes and controllers

--- backEnd/src/server.js
+++ backEnd/src/server.js
@@ -1,16 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import db from "./models/index.js";
+import PlayerRouter from "./routes/PlayerRoutes.js";
 
 dotenv.config();
 const { FE_PORT } = process.env;
 const PORT = FE_PORT;
 const app = express();
-
 app.use(express());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+await db.sequelize.sync({});
 
-await db.sequelize.sync({ alter: true });
+app.use("/", PlayerRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
